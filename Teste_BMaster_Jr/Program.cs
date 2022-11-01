74004aca32f7dab5661f02e332383ac04fe115ee
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-11-01 10:55:07 -0300

    @johnnymarcelino [Started Project (Teste Banco Master)] {nov.01.2022 at 10:55am}

---
+++ Teste_BMaster_Jr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teste_BMaster_Jr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
